05a81ee5a5d56c7144eccd306853f8a539f9d393
elbader17 <badereduardo@gmail.com>
2020-08-11 15:21:41 -0300

    make it better

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', '92.249.45.2'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'graphql'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', ''),
+        database: env('DATABASE_NAME', 'u724643927_api_laburapp_2'),
+        username: env('DATABASE_USERNAME', 'u724643927_api_laburapp_2'),
+        password: env('DATABASE_PASSWORD', 'Abc123abc123'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'b232c37a3228f85d7e2f995e09cce0af'),
